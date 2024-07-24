880ecf059b66989c6df33151e6aa479c9cc2064d
Shraman <pkpaulpostdn@gmail.com>
2024-07-25 03:18:24 +0530

    feat(add): admins can change role

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -48,7 +48,7 @@ export const ServerHeader = ({
                         
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={()=>{console.log("Manage Members Clicked");onOpen("members",{server})}} className="px-3 py-2 text-sm cursor-pointer">
                         Manage Members
                         <Users className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
