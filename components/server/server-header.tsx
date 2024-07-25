e99f57126ae50a7a6c66b77089a5a2587470a837
Shraman <pkpaulpostdn@gmail.com>
2024-07-26 03:57:55 +0530

    feat(add): create channel and leave server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -55,7 +55,7 @@ export const ServerHeader = ({
                         
                 )}
                 {isModerator && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={()=>onOpen("createChannel")} className="px-3 py-2 text-sm cursor-pointer">
                         Create Channels
                         <PlusCircle className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
@@ -70,7 +70,9 @@ export const ServerHeader = ({
                     </DropdownMenuItem>
                 )}
                 {!isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem
+                    onClick={()=>{console.log("Leave server is clicked");onOpen("leaveServer",{ server })}}
+                    className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
