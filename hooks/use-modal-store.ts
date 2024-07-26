2bdcbc9827615116449f09ecf45d1002f8b8c018
Shraman <pkpaulpostdn@gmail.com>
2024-07-27 03:28:14 +0530

    feat(add): Server Channel Member list

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,12 +1,13 @@
 "use client";
 
-import { Server } from '@prisma/client';
+import { ChannelType, Server } from '@prisma/client';
 import { create } from 'zustand';
 
 export type ModalType = "createServer"|"invite"|"editServer"|"members"|"createChannel"|"leaveServer"|"deleteServer";
 
 interface ModalData {
   server?:Server;
+  channelType?:ChannelType;
 }
 
 interface ModalStore {
