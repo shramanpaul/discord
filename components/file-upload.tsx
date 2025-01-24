d0e600dc462bd0a7970044facb10f9806198f253
Nishant-k-sagar <nishantsagar004@gmail.com>
2025-01-24 16:54:55 +0530

    Added file uploading feature

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,61 +1,89 @@
-"use client"
+"use client";
 
-import {X} from "lucide-react";
+// import { X } from "lucide-react";
 import Image from "next/image";
+import { X, FileIcon } from "lucide-react";
 
 import { UploadDropzone } from "@/lib/uploadthing";
 import { useEffect } from "react";
-// import "@uploadthing/react/styles.css"
+// import "@uploadthing/react/styles.css";
 
 interface FileUploadProps {
-    onChange:(url?: string)=> void;
-    value: string;
-    endpoint:"messageFile"| "serverImage"
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
 }
 
-export const FileUpload=({
-    onChange,
-    value,
-    endpoint
-}: FileUploadProps)=>{
-    useEffect(() => {
-        import("@uploadthing/react/styles.css")
-          .then(() => {
-            console.log("Styles imported successfully.");
-          })
-          .catch((error) => {
-            console.error("Failed to import styles:", error);
-          });
-      }, []);
+export const FileUpload = ({
+  onChange,
+  value,
+  endpoint,
+}: FileUploadProps) => {
+  useEffect(() => {
+    //@ts-ignore
+    import("@uploadthing/react/styles.css")
+      .then(() => {
+        console.log("Styles imported successfully.");
+      })
+      .catch((error) => {
+        console.error("Failed to import styles:", error);
+      });
+  }, []);
 
-    const fileType= value?.split(".").pop();
-    if(value&& fileType!=="pdf"){
-        return (
-            <div className="relative h-20 w-20">
-                <Image
-                    fill
-                    src={value}
-                    alt="Uplaod"
-                    className="rounded-full"
-                />
-                <button onClick={()=>onChange("")}
-                    className="bg-rose-500 text-whtie p-1 rounded-full absolute top-0 right-0 shadow-sm"
-                    type="button"
-                    >
-                    <X className="h-4 w-4"/>
-                </button>
-            </div>
-        )
-    }
-    return(
-        <UploadDropzone 
-        endpoint={endpoint}
-        onClientUploadComplete={(res)=>{
-            onChange(res?.[0].url)
-        }}
-        onUploadError={(error:Error)=>{
-            console.log(error);
-        }}
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full"
         />
-    )
-}
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0]?.url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
