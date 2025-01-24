e9ddfad20baf8b472e203a8f33f1cbd10a973ba8
Nishant-k-sagar <nishantsagar004@gmail.com>
2025-01-24 17:18:40 +0530

    updated the modals

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -3,17 +3,19 @@
 import { Channel, ChannelType, Server } from '@prisma/client';
 import { create } from 'zustand';
 
-export type ModalType = "createServer"|"invite"|"editServer"|"members"|"createChannel"|"leaveServer"|"deleteServer"|"deleteChannel"|"editChannel";
+export type ModalType = "createServer"|"invite"|"editServer"|"members"|"createChannel"|"leaveServer"|"deleteServer"|"deleteChannel"|"editChannel"|"messageFile"|"deleteMessage";
 
 interface ModalData {
   server?:Server;
   channel?:Channel;
   channelType?:ChannelType;
+  apiUrl?:string;
+  query?:Record<string, any>;
 }
 
 interface ModalStore {
   type: ModalType | null;
-  data: ModalData;
+  data: ModalData; 
   isOpen: boolean;
   onOpen: (type: ModalType, data?: ModalData) => void;
   onClose: () => void;
