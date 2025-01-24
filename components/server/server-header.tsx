3eb65dc7adf1c0a5065c7cd117b8c8e7e74c8e3e
Nishant-k-sagar <nishantsagar004@gmail.com>
2025-01-24 17:12:47 +0530

    Added dropdown feature in server header

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -2,83 +2,113 @@
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole, Server } from "@prisma/client";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus,
+  Users,
+} from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
-    server: ServerWithMembersWithProfiles;
-    role?: MemberRole;
+  server: ServerWithMembersWithProfiles;
+  role?: MemberRole;
 }
 
-export const ServerHeader = ({
-    server,
-    role,
-}:ServerHeaderProps
-) => {
-    const {onOpen} =useModal();
+export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
 
-    const isAdmin=role===MemberRole.ADMIN;
-    const isModerator=isAdmin || role===MemberRole.MODERATOR;
+  const isAdmin = role === MemberRole.ADMIN;
+  const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
-
-    return ( 
-        <DropdownMenu>
-            <DropdownMenuTrigger
-                className="focus: outline-none" asChild
-            >
-                <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 trasition">
-                    {server.name}
-                    <ChevronDown className="w-5 h-5 ml-auto" />
-                </button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
-                {isModerator && (
-                    <DropdownMenuItem onClick={()=>{console.log("Invite People clicked"); onOpen("invite",{ server })}} className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
-                        Invite People
-                        <UserPlus className="h-4 w-4 ml-auto"/>
-                    </DropdownMenuItem>
-                        
-                )}
-                {isAdmin && (
-                    <DropdownMenuItem onClick={()=>{console.log("Server Settings clicked"); onOpen("editServer",{server})}} className="px-3 py-2 text-sm cursor-pointer">
-                        Server Settings
-                        <Settings className="h-4 w-4 ml-auto"/>
-                    </DropdownMenuItem>
-                        
-                )}
-                {isAdmin && (
-                    <DropdownMenuItem onClick={()=>{console.log("Manage Members Clicked");onOpen("members",{server})}} className="px-3 py-2 text-sm cursor-pointer">
-                        Manage Members
-                        <Users className="h-4 w-4 ml-auto"/>
-                    </DropdownMenuItem>
-                        
-                )}
-                {isModerator && (
-                    <DropdownMenuItem onClick={()=>onOpen("createChannel")} className="px-3 py-2 text-sm cursor-pointer">
-                        Create Channels
-                        <PlusCircle className="h-4 w-4 ml-auto"/>
-                    </DropdownMenuItem>
-                )}
-                {isModerator && (
-                    <DropdownMenuSeparator />
-                )}
-                {isAdmin && (
-                    <DropdownMenuItem onClick={()=>onOpen("deleteServer",{server})} className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
-                        Delete Server
-                        <Trash className="h-4 w-4 ml-auto"/>
-                    </DropdownMenuItem>
-                )}
-                {!isAdmin && (
-                    <DropdownMenuItem
-                    onClick={()=>{console.log("Leave server is clicked");onOpen("leaveServer",{ server })}}
-                    className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
-                        Leave Server
-                        <LogOut className="h-4 w-4 ml-auto"/>
-                    </DropdownMenuItem>
-                )}
-            </DropdownMenuContent>
-        </DropdownMenu> 
-     );
-}
- 
+  return (
+<DropdownMenu>
+      <DropdownMenuTrigger className="focus: outline-none" asChild>
+        <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 trasition">
+          {server.name}
+          <ChevronDown className="w-5 h-5 ml-auto" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
+        {isModerator && (
+          <DropdownMenuItem
+            onClick={() => {
+              console.log("Invite People clicked");
+              onOpen("invite", { server });
+            }}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+          >
+            Invite People
+            <UserPlus className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {
+              console.log("Server Settings clicked");
+              onOpen("editServer", { server });
+            }}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Server Settings
+            <Settings className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {
+              console.log("Manage Members Clicked");
+              onOpen("members", { server });
+            }}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Manage Members
+            <Users className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem
+            onClick={() => onOpen("createChannel")}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Create Channels
+            <PlusCircle className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && <DropdownMenuSeparator />}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => onOpen("deleteServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {
+              console.log("Leave server is clicked");
+              onOpen("leaveServer", { server });
+            }}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+    
