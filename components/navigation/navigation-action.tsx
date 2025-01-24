2cb91e4a27e47cfc7dbae6cccc6220f9a4309976
Nishant-k-sagar <nishantsagar004@gmail.com>
2025-01-24 17:00:39 +0530

    Added closing feature after server creation

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import React from "react";
 import { Plus } from "lucide-react";
-import { ActionTooltip } from "@/components/action-tooltip";
-import { useModal } from "@/hooks/use-modal-store"; // Ensure this path is correct
 
-export const NavigationAction = () => {
+import { ActionTooltip } from "@/components/action-tooltip";
+import { useModal } from "@/hooks/use-modal-store";
+
+export function NavigationAction() {
   const { onOpen } = useModal();
 
   return (
@@ -15,10 +17,13 @@ export const NavigationAction = () => {
           className="group flex items-center"
         >
           <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
-            <Plus className="group-hover:text-white transition text-emerald-500" />
+            <Plus
+              className="group-hover:text-white transition text-emerald-500"
+              size={25}
+            />
           </div>
         </button>
       </ActionTooltip>
     </div>
   );
-};
+}
