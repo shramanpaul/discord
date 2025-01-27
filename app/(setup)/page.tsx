0d1d0712c3876e782eb580b785d03d574d5846ce
shraman <22bcs229@iiitdmj.ac.in>
2025-01-27 17:40:51 +0530

    fix: deployement issue on (api)/page.tsx

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -6,22 +6,27 @@ import { InitialModal } from "@/components/modals/initial-modal";
 const SetupPage = async () => {
     const profile = await initialProfile();
 
+    // Type check to ensure `profile` has `id`
+    if (!profile || typeof profile !== 'object' || !('id' in profile)) {
+        // Handle the case where `initialProfile` returns something unexpected
+        return <InitialModal />;
+    }
+
     const server = await db.server.findFirst({
-        where:{
-            members:{
-                some:{
+        where: {
+            members: {
+                some: {
                     profileId: profile.id,
                 },
             },
-            }
-        }
-    );
+        },
+    });
 
-    if (server){
+    if (server) {
         return redirect(`/servers/${server.id}`);
     }
 
     return <InitialModal />;
-}
- 
+};
+
 export default SetupPage;
